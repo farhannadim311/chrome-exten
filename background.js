9f9b6a9747ad91d34283e5d36ba79ac2898e5e5b
farhannadim311 <121982005+farhannadim311@users.noreply.github.com>
2024-11-06 16:12:59 -0600

    Add files via upload

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.tabs.onUpdated.addListener((tabId, tab) =>)
+{
+  if (tab.url && tab.url.includes("youtube.com/watch"))
+  {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+    chrome.tabs.sendMessage(tabId, 
+      {
+        type: "NEW",
+        videoID: urlParameters.get("v"),
+      }
+    );
+  }
+};
+  
